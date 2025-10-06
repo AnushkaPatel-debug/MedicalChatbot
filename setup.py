c735261604678c7f1e195ba49b893efaedc2376a
Anushka <anushkaa.p1710@gmail.com>
2025-10-07 00:32:58 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages , setup
+setup(
+    name="Medical Chatbot",
+    version="0.1.0",
+    author="Anushka",
+    author_email="anushkaa.p1710@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
+#REQUIRES FOR IMPORT FUNCTION FROM DIFFERENT DIFFERENT MODEL..IT WILL HELP TO INSTALL ANY KIND OF LOCAL FOLDER
